f464e35cc0fbb3084282829610aec5876a513ace
Stevan Rašković <lordsteva@hotmail.com>
2021-09-30 09:16:30 +0200

    add link compoennt, fix links in product card

---
+++ src/components/Link/Llink.tsx
@@ -0,0 +1,17 @@
+import NextLink from 'next/link';
+import { FC } from 'react';
+//TODO: add other types
+export enum LinkType {
+  Primary = 'text-transparent bg-clip-text bg-gradient-to-b from-primary-start to-primary-stop hover:from-primary-stop ',
+}
+
+type Props = {
+  to: string;
+  title?: string;
+  type?: LinkType;
+};
+
+const Link: FC<Props> = ({ to, title = '', type = LinkType.Primary, children }) => {
+  return <NextLink href={to}>{children ?? <a className={`${type}`}> {title}</a>}</NextLink>;
+};
+export default Link;
--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -3,6 +3,7 @@ import Button from '../Button';
 import { ButtonType } from '../Button/Button';
 import React, { FC } from 'react';
 import { NFTItem } from '../../types/NFTItem';
+import Link from 'components/Link/Llink';
 
 type Props = {
   item: NFTItem;
@@ -13,9 +14,13 @@ const ProductCard: FC<Props> = ({ item }) => {
     <li className="text-white bold">
       <div className="space-y-4 bg-secondary border border-gray-600 px-4 py-3 rounded-md flex flex-col justify-between h-full">
         <div className="flex items-center space-x-4 lg:space-x-6">
-          <img className="w-16 h-16 rounded-full lg:w-15 lg:h-15" src={item.ownerProfileImageUrl} alt="" />
-          <div className="font-medium text-lg leading-6 space-y-1">
-            <h3>{`@ ${item.ownerUsername}`}</h3>
+          <Link to="#">
+            <img className="w-16 h-16 rounded-full lg:w-15 lg:h-15" src={item.ownerProfileImageUrl} alt="" />
+          </Link>
+          <div className="font-medium	text-lg leading-6 space-y-1">
+            <Link to="#">
+              <h3 className="text-gray-500 hover:text-white">{`@ ${item.ownerUsername}`}</h3>
+            </Link>
           </div>
         </div>
 
@@ -30,9 +35,7 @@ const ProductCard: FC<Props> = ({ item }) => {
             <span className="px-1 text-gray-600 normal	">{`${item.availableQuantity}/${item.createdQuantity}`}</span>
           </div>
           <div className="pl-4 text-lg leading-6 font-medium flex items-end	justify-between">
-            <span className="text-transparent bg-clip-text bg-gradient-to-b from-primary-start to-primary-stop">
-              Buy Now
-            </span>
+            <Link to="#" title="Buy Now" />
             <Button
               customClasses="text-lg text-gray-600 py-0"
               icon={FavouriteIcon}
