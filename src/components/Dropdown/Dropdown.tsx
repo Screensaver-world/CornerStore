3da589c8c793af35e8080e3ea1014b767a0499dd
Stevan Rašković <lordsteva@hotmail.com>
2021-10-06 10:24:56 +0200

    fix dropdown z index

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -29,7 +29,7 @@ const Dropdown: FC<Props> = ({ displayText, dropDownContent, displayDropOnly }) 
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <div className="absolute right-0 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <div className="absolute right-0 z-10 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           {dropDownContent}
         </div>
       </Transition>
