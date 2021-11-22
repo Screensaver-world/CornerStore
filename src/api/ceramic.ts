0e9c567e0b9fb7355f2c6ab7c835001524b73649
Geo25rey <gcasper42@gmail.com>
2021-11-22 12:20:35 -0500

    fix some bugs & add caip10 info

--- src/api/ceramic.ts
+++ src/api/ceramic.ts
@@ -1,53 +1,26 @@
 import { useQuery } from 'react-query';
 
-// The Ceramic instance for a "raw" connection to the network
-import CeramicClient from '@ceramicnetwork/http-client';
-// The IDX instance for getting the "basicProfile" info, including display name
-import { IDX } from '@ceramicstudio/idx';
-// CAIP10 look up
-import { Caip10Link } from '@ceramicnetwork/stream-caip10-link';
+import { Core } from '@self.id/core';
 
-// For resolving Decentralized Identity documents
-import { DID } from 'dids';
-// import KeyDidResolver from 'key-did-resolver';
-import ThreeIdResolver from '@ceramicnetwork/3id-did-resolver';
-import { Resolver } from 'did-resolver';
-
-import type { AlsoKnownAs, BasicProfile, AlsoKnownAsAccount } from '@ceramicstudio/idx-constants';
-
-const API_URL = 'https://gateway.ceramic.network';
+import type { BasicProfile, AlsoKnownAsAccount } from '@ceramicstudio/idx-constants';
+import { addrToCaip } from 'utils/caip10';
+import { useEffect } from 'react';
 
 // init
 
-const ceramic = new CeramicClient(API_URL);
-const idx = new IDX({
-  autopin: false,
-  ceramic,
-});
-// const keyDidResolver = KeyDidResolver.getResolver();
-const threeIdResolver = ThreeIdResolver.getResolver(ceramic);
-const resolver = new Resolver({
-  ...threeIdResolver,
-  // ...keyDidResolver,
-});
-const didResolver = new DID({ resolver });
-const didSetPromise = ceramic.setDID(didResolver);
+const core = new Core({ ceramic: 'mainnet-gateway' });
 
 async function getDID(ethAddress: string | null): Promise<string | null> {
-  await didSetPromise;
-  const caip10Info = await Caip10Link.fromAccount(ceramic, ethAddress);
-  return caip10Info.did;
+  return ethAddress ? core.getAccountDID(ethAddress) : null;
 }
 
 async function getProfileInfo(did: string): Promise<BasicProfile | null> {
-  await didSetPromise;
-  const basicProfileInfo = await idx.get<BasicProfile | null>('basicProfile', did);
+  const basicProfileInfo = await core.get('basicProfile', did);
   return basicProfileInfo;
 }
 
 async function getSocials(did: string): Promise<AlsoKnownAsAccount[]> {
-  await didSetPromise;
-  const socials = await idx.get<AlsoKnownAs>('alsoKnownAs', did);
+  const socials = await core.get('alsoKnownAs', did);
   return socials.accounts || [];
 }
 
@@ -60,17 +33,31 @@ export function useProfile(ethAddress: string | null): {
   loading: boolean;
   error: unknown;
 } {
+  console.log('ethAddress', ethAddress);
+  console.log('caip', addrToCaip.eth(ethAddress || ''));
+
   const {
     data: did,
     error: didError,
     isLoading: didLoading,
-  } = useQuery(['did', ethAddress], () => getDID(ethAddress), {
-    enabled: !!ethAddress,
-  });
+    refetch: refetchDID,
+  } = useQuery(['did', 'eth', ethAddress], () => (ethAddress ? getDID(addrToCaip.eth(ethAddress)) : undefined));
 
-  const { data, isLoading, error } = useQuery(['profile', did], () => getProfileInfo(did), {
-    enabled: !!did && !didError && !didLoading,
-  });
+  useEffect(() => {
+    console.log('fetch DID');
+    refetchDID();
+  }, [ethAddress]);
+
+  console.log('did', did);
+
+  const { data, isLoading, error, refetch: refetchProfile } = useQuery(['profile', did], () => getProfileInfo(did));
+
+  useEffect(() => {
+    console.log('fetch profile');
+    refetchProfile();
+  }, [did]);
+
+  console.log('profile', data);
   return {
     basicProfileInfo: data,
     loading: isLoading || didLoading,
@@ -87,9 +74,7 @@ export function useSocials(ethAddress: string | null): {
     data: did,
     error: didError,
     isLoading: didLoading,
-  } = useQuery(['did', ethAddress], () => getDID(ethAddress), {
-    enabled: !!ethAddress,
-  });
+  } = useQuery(['did', 'eth', ethAddress], () => (ethAddress ? getDID(addrToCaip.eth(ethAddress)) : undefined));
 
   const { data, isLoading, error } = useQuery(['socials', did], () => getSocials(did), {
     enabled: !!did && !didError && !didLoading,
